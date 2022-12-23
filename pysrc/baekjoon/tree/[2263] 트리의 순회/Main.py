0d6bfec34ed2c5fa6f5d529a01f12e6e2beb9882
Ribo <ribo.ha1111@gmail.com>
2022-12-24 00:16:01 +0900

    BaekJoon: Solved Tree / 재풀이

--- pysrc/baekjoon/tree/[2263] 트리의 순회/Main.py
+++ pysrc/baekjoon/tree/[2263] 트리의 순회/Main.py
@@ -4,17 +4,16 @@ sys.setrecursionlimit(10**8)
 
 
 def getPreorder(inS, inE, postS, postE):
-    if inS > inE:
+    if inS > inE or postS > postE:
         return
     
     root = postorder[postE]
     
     left = pos[root] - inS
-    right = inE - pos[root]
-    
+
     print(root, end = " ")
-    getPreorder(inS, inS+left-1, postS, postS+left-1)
-    getPreorder(inE-right+1, inE, postE-right, postE-1)
+    getPreorder(inS, pos[root]-1, postS, postS+left-1)
+    getPreorder(pos[root]+1, inE, postS+left, postE-1)
 
 n = int(input())
 inorder = list(map(int, input().split()))
