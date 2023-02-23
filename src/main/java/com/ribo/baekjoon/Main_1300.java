977afc6853b0a33440043c03070ddc94389ba9a3
Ribo <ribo.ha1111@gmail.com>
2023-02-23 22:18:10 +0900

    BaekJoon: Solved Binary Search

---
+++ src/main/java/com/ribo/baekjoon/Main_1300.java
@@ -0,0 +1,38 @@
+package com.ribo.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main_1300 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+
+        long start = 1;
+        long end = K;
+
+        // lower-bound
+        while(start < end) {
+
+            long mid = (start + end) / 2;
+            long count = 0;
+
+            for(int i = 1; i <= N; i++) {
+                count += Math.min(mid/i, N);
+            }
+
+            if(K <= count) {
+                end = mid;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+
+        System.out.println(start);
+    }
+}
